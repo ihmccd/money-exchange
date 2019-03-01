72df3fc542dc00d06b9b1ea1d0dfeafd0addf9a2
ihmccd <igbus@mail.ru>
2019-03-02 00:52:51 +0300

    fix: solution

--- src/index.js
+++ src/index.js
@@ -2,34 +2,39 @@
 module.exports = function makeExchange(currency) {
 
   let answer = {};
-
+  
   if (currency > 10000){
     answer["error"]="You are rich, my friend! We don't have so much coins for exchange";
   }
   
-  else if(currency/50-currency%50 >0){
-    answer["H"] = Math.round(currency/50);
-    currency = currency%50;
+ else{
+   
+
+     
+   if(Math.ceil(currency/50) >0){
+    answer["H"] = Math.ceil(currency/50);
+   currency = currency%50;
   }
    
-  else if(currency/25-currency%25>0){
-    answer["Q"] = Math.round(currency/50);
+  if(Math.ceil(currency/25)>0){
+    answer["Q"] = Math.round(currency/25);
     currency = currency%25;
   }
   
-  else if(currency/10-currency%10>0){
+    if(Math.ceil(currency/10)>0){
     answer["D"] = Math.round(currency/10);
     currency = currency%10;
   }
   
-  else if(currency/5-currency%5>0){
+    if(Math.ceil(currency/5)>0){
     answer["D"] = Math.round(currency/5);
     currency = currency%5;
   }
   
-  else if(currency>0){
+  if(currency>0){
     answer["P"] = currency;
   }
+}
   
   return answer;
     // Your code goes here!
